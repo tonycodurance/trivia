6f7b34cb8c6804881f175323514ce0008ef940d2
safoura.boyot@gmail.com <safoura.boyot@gmail.com>
2018-04-18 13:32:26 +0100

    for resolving confilict

---
+++ C#/Trivia/TriviaTests/GameCoinsWinningConditionShould.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Trivia;
+
+namespace TriviaTests
+{
+    [TestFixture]
+    public class GameCoinsWinningConditionShould
+    {
+        [TestCase(6, true)]
+        [TestCase(5, false)]
+        [TestCase(1, false)]
+        public void IdentifyWinningCondition(int playerCoins, bool expectedPlayerIsWinner)
+        {
+            var gameConditions = new GameCoinsWinningCondition();
+
+            var actualPlayerIsWinner = gameConditions.CurrentPlayerWinner(playerCoins);
+
+            Assert.That(actualPlayerIsWinner, Is.EqualTo(expectedPlayerIsWinner));
+        }
+    }
+}
