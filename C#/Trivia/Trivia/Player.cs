5978e1d6d62defb0fcc7a3ea8c98c42961b48899
safoura.boyot@gmail.com <safoura.boyot@gmail.com>
2018-04-18 14:42:06 +0100

    use Players.GetNextPlayer() for Getting current player in Game

---
+++ C#/Trivia/Trivia/Player.cs
@@ -0,0 +1,20 @@
+﻿namespace Trivia
+{
+    public class Player
+    {
+        private readonly string _playerName;
+
+        public Player(string playerName)
+        {
+            _playerName = playerName;
+            Coins = 0;
+        }
+
+        public int Coins { get; private set; }
+
+        public void AddCoin()
+        {
+            Coins++;
+        }
+    }
+}
