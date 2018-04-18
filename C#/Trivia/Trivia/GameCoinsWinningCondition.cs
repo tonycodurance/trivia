ba9236ba7c4e85a5a8c7d8235203fdde86f642d3
tony <tony.georgiadis@codurance.com>
2018-04-18 12:31:41 +0100

    Add GameCoinsWinningCondition class - encapsulating the winning conditions logic in it, based on coins as the current version of the game needs - create unit tests and move test case accordingly

---
+++ C#/Trivia/Trivia/GameCoinsWinningCondition.cs
@@ -0,0 +1,19 @@
+﻿using static Trivia.WinningConditions;
+
+namespace Trivia
+{
+    public class GameCoinsWinningCondition
+    {
+        private WinningConditions _numberOfCoinsToWin = WinningCoins;
+
+        public bool CurrentPlayerWinner(int playerCoins)
+        {
+            return playerCoins == (int)_numberOfCoinsToWin;
+        }
+    }
+    
+    public enum WinningConditions
+    {
+        WinningCoins = 6
+    }
+}
